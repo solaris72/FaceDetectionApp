25ca43a77dcd1198b46d9f3f2397caf9e5128d09
Michael Muniz <michael.muniz@saharalasvegas.com>
2020-07-27 14:55:15 -0700

    More Detail

--- FaceDetectionApp/Form1.Designer.cs
+++ FaceDetectionApp/Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.pic = new System.Windows.Forms.PictureBox();
             this.btnDetect = new System.Windows.Forms.Button();
+            this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
             this.SuspendLayout();
             // 
             // pic
             // 
-            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pic.Location = new System.Drawing.Point(12, 12);
+            this.pic.Location = new System.Drawing.Point(12, 67);
             this.pic.Name = "pic";
             this.pic.Size = new System.Drawing.Size(529, 426);
             this.pic.TabIndex = 0;
@@ -44,25 +45,49 @@
             // 
             // btnDetect
             // 
-            this.btnDetect.Location = new System.Drawing.Point(547, 12);
+            this.btnDetect.Location = new System.Drawing.Point(547, 67);
             this.btnDetect.Name = "btnDetect";
             this.btnDetect.Size = new System.Drawing.Size(75, 23);
             this.btnDetect.TabIndex = 1;
             this.btnDetect.Text = "&Detect";
             this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // cboDevice
+            // 
+            this.cboDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDevice.FormattingEnabled = true;
+            this.cboDevice.Location = new System.Drawing.Point(142, 30);
+            this.cboDevice.Name = "cboDevice";
+            this.cboDevice.Size = new System.Drawing.Size(386, 21);
+            this.cboDevice.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Device:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(635, 451);
+            this.ClientSize = new System.Drawing.Size(635, 544);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboDevice);
             this.Controls.Add(this.btnDetect);
             this.Controls.Add(this.pic);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Face Detection";
+            this.Text = "Face Detection using Webcam";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +95,8 @@
 
         private System.Windows.Forms.PictureBox pic;
         private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.ComboBox cboDevice;
+        private System.Windows.Forms.Label label1;
     }
 }
 
